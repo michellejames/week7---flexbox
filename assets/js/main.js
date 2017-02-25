403fe397df65c3af5c752b4e6a408618e81b6917
michellejames <shellyjames90@yahoo.com>
2017-02-25 17:16:39 -0500

    week7-flexbox

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+console.log("Hello World from main.js!");
+
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+	$( ".menu" ).slideToggle( "slow", function() {
+		$( ".hamburger" ).hide();
+		$( ".cross" ).show();
+	});
+});
+
+$( ".cross" ).click(function() {
+	$( ".menu" ).slideToggle( "slow", function() {
+		$( ".cross" ).hide();
+		$( ".hamburger" ).show();
+	});
+});
